c59b39e76843e2b8aaf44653edaef5341d302f46
Muhammad Khan <mhk98@cornell.edu>
2015-08-02 15:11:12 -0400

    forgot about options in commands

--- src/texdocument.py
+++ src/texdocument.py
@@ -3,23 +3,26 @@ class LaTeXCommand:
     Represents the object represented by a <command>
     in a *_structure.xml file
     """
-    def __init__(self, name, contents):
+    def __init__(self, name, option="", contents):
         """
         Parameters: name - the name of the LaTeX command
+                    option - optional parameter for command
                     contents - a list of the arguments
                                to the command
 
         Example: <command label='cmd' args='3'>
+                   <option>opt</option>
                    <argument>arg1</argument>
                    <argument>arg2</argument>
                    <argument>arg3</argument>
                  </command>
 
-                 will be represented as LaTeXCommand('cmd', [arg1,arg2,arg3])
+                 will be represented as LaTeXCommand('cmd', opt,[arg1,arg2,arg3])
                  and corresponds to a line in LaTeX as follows:
-                 \cmd{arg1}{arg2}{arg3}
+                 \cmd[opt]{arg1}{arg2}{arg3}
         """
         self.name = name
+        self.option = option
         self.contents = contents
 
 class Body:
