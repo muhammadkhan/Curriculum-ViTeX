1c9b658d8aca9a495957f8c7a25416c4aa5bf420
Muhammad Khan <mhk98@cornell.edu>
2015-08-04 00:15:15 -0400

    iite so I guess it's all written o_o let the tests begin

--- src/latex_writer.py
+++ src/latex_writer.py
@@ -25,8 +25,21 @@ def commandStr(cmd):
         c += "{" + x + "}"
     return c
 
-def environmentStr(env):
+def environmentWrite(fileObj, env):
+    fileObj.write("\\begin")
+    if len(env.option) > 0:
+        fileObj.write("[" + env.option + "]")
+    fileObj.write("{" + env.name + "}")
     
+    for obj in env.cmds_and_envs:
+        if type(obj) is LaTeXCommand:
+            fileObj.write(commandStr(obj) + "\n")
+        elif type(obj) is str:
+            fileObj.write(obj + "\n")
+        elif type(obj) is Environment:
+            environmentWrite(fileObj, obj)
+    
+    fileObj.write("\\end{" + env.name + "}\n")
 
 def writeLaTeX(texdoc, cls):
     # in structureParser.py, texdoc's 'filepath'
@@ -40,11 +53,11 @@ def writeLaTeX(texdoc, cls):
         texfile.write("\\begin{document}\n")
 
         for content in texdoc.documentBody.contents:
-            if type(content) is texdocument.LaTeXCommand:
+            if type(content) is LaTeXCommand:
                 texfile.write(commandStr(content) + "\n")
             elif type(content) is str:
                 texfile.write(content + "\n")
-            elif type(content) is texdocument.Environment:
-                texfile.write(environmentStr(content) + "\n")
+            elif type(content) is Environment:
+                envrionmentWrite(texfile, content)
         
         texfile.write("\\end{document}")
